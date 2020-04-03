db1bcd59218efd88c92fbd650908ac5b49a1462b
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-03 21:41:18 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :partner, :name
+  attr_accessor  :partner, :name
 
   def initialize(name)
     @name = name
@@ -7,15 +7,18 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
-  end
+    if person.class != Person
+        raise PartnerError
+      else
+        person.partner = self
+      end
+    end
 
+  class PartnerError < StandardError
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
